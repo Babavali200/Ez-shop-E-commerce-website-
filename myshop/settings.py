5c745aab53ab3c9be69a5942388ae02a8a461cec
Babavali200 <shaikbabavali2003@gmail.com>
2026-05-07 20:41:37 +0530

    Update ALLOWED_HOSTS for security configuration

--- myshop/settings.py
+++ myshop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-placeholder')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['Ez shop  E-commerce  platform','*']
 
 # Application definition
 INSTALLED_APPS = [
